02ac2663ed9838e51d2377530691bab23370e4eb
Phil Runninger (mac) <prunninger@vhtcx.com>
2018-05-09 14:53:05 -0400

    Add error checking to permutations/2.

--- combinatorics.erl
+++ combinatorics.erl
@@ -8,6 +8,8 @@ combinations(N,[H|T]=S) -> [[H|L] || L <- combinations(N-1,S)]++combinations(N,T
 permutations([]) -> [[]];
 permutations(L)  -> [[H|T] || H <- L, T <- permutations(L--[H])].
 
+permutations(L,N) when length(L) < N ->
+    error;
 permutations(L,N) ->
     lists:usort(
       lists:map(fun(I) ->
